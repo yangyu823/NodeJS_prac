162302f1e4f76b493eb3546b836c87fa900ca089
yangyu823 <yangyu823@gmail.com>
2019-03-15 12:15:53 +1100

    testing update

--- test/test_01.js
+++ test/test_01.js
@@ -17,19 +17,31 @@ describe('Test', function() {
     });
 });
 
-describe('/GET hello', () => {
-    it('it should GET hello message', (done) => {
+describe('Hello world testing', () => {
+    it('Basic Hello world API testing', (done) => {
         chai.request('http://localhost:9999')
             .get('/hello')
             .end((err, res) => {
                 // console.log(res.text)
                 // chai.expect(res.status).to.equal(200);
-                chai.expect(res.text).to.equal("Hello world");
+                // chai.expect(res.text).to.equal("Hello world");
 
                 expect(res).to.have.status(200)
                 expect(res.text).to.equal("Hello world")
                 // res.should.have.status(200);
                 // res.body.should.be.a('string');
+                done();
+            })
+    })
+});
+
+describe('Bike API Testing', () => {
+    it('API should return json format project', (done) => {
+        chai.request('http://localhost:9999')
+            .get('/panigale/1000')
+            .end((err, res) => {
+                expect(res).to.have.status(200)
+                expect(res.body).to.have.property("capacity").with.valueOf("1000")
                 done();
             })
     })
